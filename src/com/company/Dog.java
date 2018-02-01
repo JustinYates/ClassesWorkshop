74cacd70f81a3363754b716b6ffbe53b449e204b
JustinYates <Justin.yates24601@gmail.com>
2018-02-01 15:53:26 -0500

    newest

---
+++ src/com/company/Dog.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.Scanner;
+
+public class Dog extends Pet {
+
+    private boolean likesWalks;
+    private boolean canFetch;
+    private String coatType;
+    private Scanner input = new Scanner(System.in);
+
+    public Dog() { /*(boolean likesWalks, boolean canFetch, String coatType) {
+        this.likesWalks = likesWalks;
+        this.canFetch = canFetch;
+        this.coatType = coatType;*/
+    }
+
+    public boolean isLikesWalks() {
+        return likesWalks;
+    }
+
+    public void setLikesWalks(boolean likesWalks) {
+        this.likesWalks = likesWalks;
+    }
+
+    public boolean isCanFetch() {
+        return canFetch;
+    }
+
+    public void setCanFetch(boolean canFetch) {
+        this.canFetch = canFetch;
+    }
+
+    public String getCoatType() {
+        return coatType;
+    }
+
+    public void setCoatType(String coatType) {
+        this.coatType = coatType;
+    }
+}
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
 public class Main {
+    private String MyString;
+
+    protected String lotsOfStrings;
+
+    public String lastString;
+
+    String anotherString;
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    private void aClass() {
+
     }
 }
