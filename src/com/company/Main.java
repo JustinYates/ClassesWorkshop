3578d3aeb8a333d5e36bba27983ee13c755bec96
JustinYates <Justin.yates24601@gmail.com>
2018-02-02 16:31:51 -0500

    updated minor changes?, still need to make the class execute.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,12 @@ public class Main {
 //
 //        System.out.println(dog.makeSound());
 
-        Cat cat = new Cat("Person", "Kitty", "124 main street", 5, 'F',
-                true, true, "White");
+        //Cat cat = new Cat("Person", "Kitty", "124 main street", 5, 'F',
+              //  true, true, "White");
 
-        System.out.println(cat.makeSound());
-        System.out.println(cat.getGender());
-
+       // System.out.println(cat.makeSound());
+        //System.out.println(cat.getGender());
     }
+AgeCalculator AgeCalculator = new AgeCalculator("Jay", "diamond", "3017 university rd", 0, 'F', true, true, "Black");
 
 }
